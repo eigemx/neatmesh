dbafc7326a0d400c23e3b05322021ff68ea8c462
EigenEmara <eigen.emara@gmail.com>
2022-03-08 12:11:05 +0200

    More quality.py functionality

--- src/neatmesh/quality.py
+++ src/neatmesh/quality.py
@@ -23,6 +23,8 @@ class MeshQuality3D:
         self.cells_volumes = np.zeros(shape=(self.n_cells,))
 
         self.calc_cell_types_counts()
+        self.calc_faces_data()
+        self.calc_cells_data()
 
     def calc_cell_types_counts(self) -> None:
         self.hex_count = sum(
@@ -69,73 +71,93 @@ class MeshQuality3D:
             ]
         )
 
+    @staticmethod
+    def calc_face_data(
+        face: Tuple[int, ...], mh: MeshioHandler3D
+    ) -> Tuple[float, float, float]:
+        face_size = len(face)
+        vertices = [mh.points[i] for i in face]
+        geo_center = np.sum(vertices, axis=0) / face_size
 
-def face_data(face: Tuple[int, ...], mh: MeshioHandler3D) -> Tuple[float, float, float]:
-    face_size = len(face)
-    vertices = [mh.points[i] for i in face]
-    geo_center = np.sum(vertices, axis=0) / face_size
+        face_total_area = 0.0
+        face_normal_vector = np.zeros((3,))
+        face_centroid = np.zeros((3,))
 
-    face_total_area = 0.0
-    face_normal_vector = np.zeros((3,))
-    face_centroid = np.zeros((3,))
+        # form the traingular subfaces.
+        # each face is constructed using an edge as the base of
+        # the triangle and geometric center as its apex.
+        for i in range(face_size):
+            # Set the subface points
+            p1 = vertices[i]
+            p2 = vertices[(i + 1) % face_size]
+            p3 = geo_center
 
-    # form the traingular subfaces.
-    # each face is constructed using an edge as the base of
-    # the triangle and geometric center as its apex.
-    for i in range(face_size):
-        # Set the subface points
-        p1 = vertices[i]
-        p2 = vertices[(i + 1) % face_size]
-        p3 = geo_center
+            # calculate the subface geometric center
+            subface_geo_center = np.sum([p1, p2, p3], axis=0) / 3.0
 
-        # calculate the subface geometric center
-        subface_geo_center = np.sum([p1, p2, p3], axis=0) / 3.0
+            # calculate the area and normal vector 'sf' for the subface
+            sf = np.cross((p2 - p1), (p3 - p1))
+            area = np.linalg.norm(sf) / 2.0
 
-        # calculate the area and normal vector 'sf' for the subface
-        sf = np.cross((p2 - p1), (p3 - p1))
-        area = np.linalg.norm(sf) / 2.0
+            face_normal_vector += sf
+            face_total_area += area
+            face_centroid = face_centroid + (area * subface_geo_center)
 
-        face_normal_vector += sf
-        face_total_area += area
-        face_centroid = face_centroid + (area * subface_geo_center)
+        face_centroid /= face_total_area
+        face_normal_vector /= 2.0
 
-    face_centroid /= face_total_area
-    face_normal_vector /= 2.0
+        return (face_centroid, face_total_area, face_normal_vector)
 
-    return (face_centroid, face_total_area, face_normal_vector)
+    def calc_faces_data(self) -> None:
+        for i, face in enumerate(self.mh.faces):
+            face_centroid, face_total_area, face_normal_vector = self.calc_face_data()
+            self.faces_centers[i,:] = face_centroid
+            self.faces_areas[i] = face_total_area
+            self.faces_normals[i,:] = face_normal_vector
+            
+            if len(face) == 3:
+                self.n_tri_faces += 1
+            else:
+                self.n_quad_faces += 1
 
+    def calc_cell_data(
+        self, cell: Tuple[int, ...], cell_type: MeshIOCellType, mh: MeshioHandler3D
+    ) -> Tuple[float, float, float]:
+        geo_centeroid = np.zeros((3,))
 
-def cell_data(
-    cell: Tuple[int, ...], cell_type: MeshIOCellType, mh: MeshioHandler3D
-) -> Tuple[float, float, float]:
-    geo_centeroid = np.zeros((3,))
+        cell_faces = cell_type_handler_map[cell_type](cell)
 
-    cell_faces = cell_type_handler_map[cell_type](cell)
-    for face in cell_faces:
-        face_id = mh.face_to_faceid[frozenset(face)]
-        geo_centeroid += face.center
-    geo_centeroid /= len(self.faces)
+        for face in cell_faces:
+            face_id = mh.face_to_faceid[frozenset(face)]
+            geo_centeroid += self.faces_centers[face_id, :]
 
-    # construct a pyramid with each cell face as the base and geometric center as the apex.
-    element_volume = 0.0
-    element_centroid = np.zeros((3,))
+        geo_centeroid /= len(self.faces)
 
-    for face in self.cell_faces:
-        face_centroid, face_area, _ = face.center_and_area()
+        # construct a pyramid with each cell face as the base and geometric center as the apex.
+        element_volume = 0.0
+        element_centroid = np.zeros((3,))
 
-        # pyramid centroid is located on 0.25 the distance between the base and the apex.
-        pyramid_centroid = (0.75 * face_centroid) + (0.25 * geo_centeroid)
-        pyramid_volume = (
-            (1.0 / 3.0) * face_area * np.linalg.norm(face_centroid - geo_centeroid)
-        )
+        for face in self.cell_faces:
+            face_id = mh.face_to_faceid[frozenset(face)]
+            face_centroid = self.faces_centers[face_id, :]
+            face_area = self.faces_areas[face_id]
 
-        element_volume += pyramid_volume
-        # element_centroid will be divided by total volume after the end of faces loop
-        element_centroid += pyramid_volume * pyramid_centroid
+            # pyramid centroid is located on 0.25 the distance between the base and the apex.
+            pyramid_centroid = (0.75 * face_centroid) + (0.25 * geo_centeroid)
+            pyramid_volume = (
+                (1.0 / 3.0) * face_area * np.linalg.norm(face_centroid - geo_centeroid)
+            )
 
-    # Finally, calculate the volume weighted element centre.
-    element_centroid = element_centroid / element_volume
+            element_volume += pyramid_volume
 
-    self.center, self.volume = element_centroid, element_volume
+            # element_centroid will be divided by total volume after the end of faces loop
+            element_centroid += pyramid_volume * pyramid_centroid
 
-    return element_centroid, element_volume
+        # Finally, calculate the volume weighted element centre.
+        element_centroid = element_centroid / element_volume
+
+        return element_centroid, element_volume
+
+    def calc_cells_data(self) -> None:
+        for cell in self.mh.cells():
+            
