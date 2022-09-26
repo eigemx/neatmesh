5ac44c190b6dc580d4586e949023cc03104bfbcb
EigenEmara <eigen.emara@gmail.com>
2022-09-26 19:03:55 +0200

    expose exceptions to public API

--- src/neatmesh/__init__.py
+++ src/neatmesh/__init__.py
@@ -1,3 +1,14 @@
+from ._exceptions import (
+    InputMeshDimensionError,
+    InvalidMeshError,
+    NonSupportedElementError,
+)
 from ._mesh import Mesh2D, Mesh3D
 
-__all__ = ["Mesh2D", "Mesh3D"]
+__all__ = [
+    "Mesh2D",
+    "Mesh3D",
+    "InputMeshDimensionError",
+    "InvalidMeshError",
+    "NonSupportedElementError",
+]
--- tests/test_single_quad.py
+++ tests/test_single_quad.py
@@ -18,12 +18,12 @@ def test_single_quad_2d_mesh():
     assert mesh.n_quad == 1
     assert mesh.n_faces == 1
     assert mesh.n_tri == 0
-    
+
     normals = mesh.edge_normals
     assert np.dot(normals[0], normals[1]) == 0
     assert np.dot(normals[2], normals[3]) == 0
-    assert np.isclose(np.sum(mesh.face_areas), 1.)
+    assert np.isclose(np.sum(mesh.face_areas), 1.0)
     assert np.allclose(mesh.face_centers, np.array([[0.5, 0.5]]))
-    
+
     assert not np.all(mesh.internal_edges_mask)
     assert np.all(mesh.boundary_edges_mask)
